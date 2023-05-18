5d8dd0640a8b255d4e5faccce15a62f436ec09b2
dipdowel <leonid.bogouslavski@gmail.com>
2023-05-18 14:19:34 +0200

    Don't commit `dist` for now

--- parcel-transformer-assemblyscript-codument/dist/helpers/throw-transformer-error.d.ts
+++
@@ -1,6 +0,0 @@
-import { ErrorObj } from "../default-error";
-/**
- * Throws an error in the Parcel way.
- * @param error
- */
-export declare function throwTransformerError(error: ErrorObj): void;
--- parcel-transformer-assemblyscript-codument/dist/helpers/write-declaration-file.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Write types for the WASM module to the disk.
- * The `.d.ts` file should contain all the signatures of callable function and accessible properties of the WASM module.
- * @param content
- */
-export declare function writeDeclarationFile(content: string): void;
