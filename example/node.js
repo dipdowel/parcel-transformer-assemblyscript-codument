853c435f88caccb88069f8df40b62c6912d5cb63
dipdowel <leonid.bogouslavski@gmail.com>
2023-05-29 16:34:37 +0200

    experimental: trying to test `io`

--- example/node.js
+++ example/node.js
@@ -1,9 +1,14 @@
+/***************************************************************************************************
+ * NB: Currently parcel-transformer-assemblyscript-codument does not
+ * have a working example for NodeJS. This is a placeholder file.
+ * Making AssemblyScript work with Parcel is planned for the future.
+ */
+
 // import wasmString from "bundle-text:./test.wat";
 // import wasmString from "./index.as";
-import * as wasmBytes from "./assembly/index.as.ts";
-
-console.log(`>>> wasmBytes: ${wasmBytes}`);
-
+// import * as wasmBytes from "./assembly/index.as.ts";
+//
+// console.log(`wasmBytes: ${wasmBytes}`);
 //
 // var wasmBytes = new TextEncoder().encode(wasmString);
 //
